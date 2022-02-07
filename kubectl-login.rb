d5acb247103c5d9b91b6aba27adafcb6203ac066
devopstales <42894256+devopstales@users.noreply.github.com>
2022-02-07 16:53:28 +0100

    kubectl-login_0.1

--- kubectl-login.rb
+++ kubectl-login.rb
@@ -4,7 +4,7 @@ class KubectlLogin < Formula
   url "https://github.com/devopstales/kube-openid-connect/releases/download/client_v0.1/kubectl-login_linux.tar.gz"
   sha256 "da922b3c845af81105c2ecf6b9109ef942d0d1616bba88d00272f9657a620d65"
   license "Apache-2.0"
-  version: 0.1
+  version: "0.1"
 
   def install
     bin.install "kubectl-login"
