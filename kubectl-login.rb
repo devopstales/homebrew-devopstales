9a2403a713d4c51bb1921d79317be9004b89dfc4
devopstales <42894256+devopstales@users.noreply.github.com>
2022-02-20 11:36:32 +0100

    release 0.2

--- kubectl-login.rb
+++ kubectl-login.rb
@@ -2,14 +2,14 @@ class KubectlLogin < Formula
   desc "An application that can be used to easily enable authentication flows via OIDC for a kubernetes cluster. "
   homepage "https://github.com/devopstales/kube-openid-connect/"
   license "Apache-2.0"
-  version "0.1"
+  version "0.2"
 
   if OS.mac?
-    url "https://github.com/devopstales/kube-openid-connect/releases/download/client_v0.1/kubectl-login_osx.tar.gz"
-    sha256 "7c658099fa209b738d01a0b0d466181d20729ba3511033dbf9a94e342edcb206"
+    url "https://github.com/devopstales/kube-openid-connect/releases/download/client_v0.2/kubectl-login_osx.tar.gz"
+    sha256 "6f6f64c90fc1359508cf9c1429788ecf6f75ee5127bd9020c250043787b87afc"
   elsif OS.linux?
-    url "https://github.com/devopstales/kube-openid-connect/releases/download/client_v0.1/kubectl-login_linux.tar.gz"
-    sha256 "e33877fab8ea54626089ef37db712e92be4e0f22df23971e80321857f496ad3c"
+    url "https://github.com/devopstales/kube-openid-connect/releases/download/client_v0.2/kubectl-login_linux.tar.gz"
+    sha256 "dd01b92f1f33194359d1c5b172adcd7ac938609028fc449dd4c84c9f93a3e4b9"
   end
 
   def install
