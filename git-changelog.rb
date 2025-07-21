11e94b17a00879b107a84acb241a85a982b7a2f9
devopstales <42894256+devopstales@users.noreply.github.com>
2025-07-21 14:32:43 +0200

    Update git-changelog.rb

--- git-changelog.rb
+++ git-changelog.rb
@@ -1,6 +1,6 @@
 class GitChangelog < Formula
   url "https://github.com/devopstales/homebrew-devopstales/archive/refs/tags/git-changelog-1.0.tar.gz"
-  sha256 "7742c29038118f959c8207657b9327d7cd5a63eaa50c03d6d34c83a388f2e31f"
+  sha256 "e7cd4d00943f5f4cd7bedd40ccf989639fc23a9cc0fff1fc5c8535bab66523a1"
   version "1.0"
 
   depends_on "git"
